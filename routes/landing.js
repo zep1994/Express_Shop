eb1e09a45e59130c4bcd7d5ec7efb6198a1b7ad6
zep1994 <snaxbags@protonmail.com>
2019-11-01 07:12:13 -0400

    fix routing error

--- routes/landing.js
+++ routes/landing.js
@@ -4,7 +4,8 @@ const path = require('path')
 const router = express.Router()
 
 router.get('/', (req, res, next) => {
-    res.sendFile(path.join(__dirname, "views", "landing.html"))
+    //res.sendFile(path.join(__dirname, "views", "landing.html"))
+    res.send('<h1>Landing</h1>')
 })
 
 module.exports = router
